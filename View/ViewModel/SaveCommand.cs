88cbb8f76421f2431411bc9ff16a2345407da32e
Сергей Масликов <smaslikova7240@gmail.com>
2025-02-27 19:26:46 +0700

    создан класс SaveCommand.cs

--- View/ViewModel/SaveCommand.cs
+++ View/ViewModel/SaveCommand.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Input;
+using View.Model;
 
 namespace View.ViewModel
 {
-    internal class SaveCommand
+    /// <summary>
+    /// Класс команды сохранения данных контакта в файл.
+    /// </summary>
+    public class SaveCommand : ICommand
     {
+        /// <summary>
+        /// Событие, вызываемое при изменении условий выполнения команды.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Выполняет команду сохранения контакта в файл "contact_save.txt".
+        /// </summary>
+        /// <param name="parameter">Ожидается объект типа Contact.</param>
+        public void Execute(object parameter)
+        {
+            if (parameter is Contact contact)
+            {
+                string filePath = "contact_save.txt";
+                try
+                {
+                    File.WriteAllText(filePath, $"{contact.Name}\n{contact.PhoneNumber}\n{contact.Email}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка сохранения: {ex.Message}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Определяет, может ли команда выполняться.
+        /// </summary>
+        /// <param name="parameter">Параметр команды.</param>
+        /// <returns>Всегда возвращает true.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
     }
 }
